2350d3ecddca58f54c0f51f10566bb1e88b9dbba
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-05-10 22:53:42 +0530

    second commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,24 +1,31 @@
-import '@styles/global.css';
+import "@styles/global.css";
+
+import Nav from "@components/Nav";
+
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
-    description: "Discover & Share AI Prompts"
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-        <body>
-            <div className="main">
-                <div className="gradient" />
-            </div>
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-            <main className="app">
-                { children }
-            </main>
-        </body>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
