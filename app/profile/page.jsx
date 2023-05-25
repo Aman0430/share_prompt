ae60bbb59643b7bc00cf6c95f15d9ab4da1e3603
Aman0430 <112618669+Aman0430@users.noreply.github.com>
2023-05-25 23:32:12 +0530

    eighth commit

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -27,12 +27,28 @@ const MyProfile = () => {
     
 
     const handleEdit = (post) => {
-      router.push(`/update-prompt?id=${post._id}`)
-    }
+      router.push(`/update-prompt?id=${post._id}`);
+    };
     
     const handleDelete = async (post) => {
-
-    }
+      const hasConfirmed = confirm(
+        "Are you sure you want to delete this prompt?"
+      );
+  
+      if (hasConfirmed) {
+        try {
+          await fetch(`/api/prompt/${post._id.toString()}`, {
+            method: "DELETE",
+          });
+  
+          const filteredPosts = posts.filter((item) => item._id !== post._id);
+  
+          setposts(filteredPosts);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
 
   return (
     <Profile 
